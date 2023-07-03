6098d55cbe51e65559d69573d661b4d63f4a9e7a
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-07-03 17:39:17 +0200

    fix import axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script>
+
 import AppMain from './components/AppMain.vue';
 import axios from 'axios';
+
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?eq[type1]=Electric&sort[number]=desc';
+
 export default {
   components: { AppMain },
   data() {
@@ -11,13 +14,14 @@ export default {
   },
   created() {
     axios.get(endpoint).then(res => {
-      this.characters = res.data.results;
+      this.characters = res.data.docs;
     });
   }
 };
 </script>
 
 <template>
+  <h1>POKEDESC</h1>
   <AppMain />
 </template>
 
